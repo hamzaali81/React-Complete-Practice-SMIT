ff2d02c10ef12a52c9d9d7e742c6b0477330433c
hamza ali <hamza54834@gmail.com>
2020-09-11 05:15:45 -0700

    more solution provide

---
+++ practice 1-14/search app/screen2 more solution/Search/Search.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      list: ['Beef Pulao', 'Chapli Kabab', 'Kaleji', 'Gola Kabab', 'Pae', 'Reshmi Kabab', 'Behari Boti', 'Dhaga Kabab', 'Hunter Beef', 'Sajji'],
+      result:[]
+    }
+    this.search=this.search.bind(this)
+  }
+  
+
+search(e){
+  const {list}=this.state;
+  console.log(e.target.value);
+  const text = e.target.value;
+
+
+    const result=list.filter(item=>{
+      const lowerItem= item.toLowerCase();
+      const lowerText=text.toLowerCase();
+      //solution 1
+    // return lowerItem.substring(0, lowerText.length).indexOf(lowerText) !== -1
+    
+    //solution 2
+    const sub =lowerItem.substring(0, lowerText.length);
+    return sub.includes(lowerText)
+      
+  })
+  this.setState({
+    result,text
+  })
+  
+}
+
+
+renderHeader(){
+  return(
+    <input type="text" placeholder="Khao Peo Jan Banao List" onChange={this.search}/>
+  )
+}
+
+  renderBody(){
+        const {list,result,text}=this.state;
+        const items= text ? result : list 
+
+     return(
+       <div>
+            {text && <h1>Khao peo jan banao list ==== "{text}"</h1>}
+            <ol>
+              {items.map((el,index)=>{
+               return <li key={index}>{el}</li>
+              })}
+            </ol>
+       </div>
+     )
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        {this.renderBody()}
+      </div>
+    );
+  }
+}
+
+export default Search;
