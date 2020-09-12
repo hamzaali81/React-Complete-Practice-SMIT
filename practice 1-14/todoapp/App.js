7777653ac2cff27e7083737c531dbde001c0b94e
hamza ali <hamza54834@gmail.com>
2020-09-12 08:09:50 -0700

    todo app complete

---
+++ practice 1-14/todoapp/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Search from './screen/Search/Search'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Search />
+      </div>
+    );
+  }
+}
+
+export default App;
