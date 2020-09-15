11ca183077c7a75917b151e3e9e2d09926dbc6bb
hamza ali <hamza54834@gmail.com>
2020-09-15 00:55:22 -0700

    life cycle hooks part 02

---
+++ practice 1-14/life cycle hooks/part2/screen/AddForm/AddForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class AddForm extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      
+    }
+
+    }
+ 
+  
+
+
+
+  render() {
+    return (
+
+      <div>
+      <h1>Add Form</h1>
+      </div>
+    );
+  }
+
+}
+export default AddForm;
